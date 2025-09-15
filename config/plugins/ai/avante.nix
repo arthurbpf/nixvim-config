ee045338b9a7b4f803cdca41efd48f5e83397f10
Arthur Fernandes <arthurbpfernandes@gmail.com>
2025-09-15 14:31:31 -0300

    update

--- config/plugins/ai/avante.nix
+++ config/plugins/ai/avante.nix
@@ -6,7 +6,7 @@ _: {
       providers = {
         ollama = {
           endpoint = "http://127.0.0.1:11434";
-          model = "qwen3:30b-a3b";
+          model = "qwen3-coder:30b-a3b-q4_K_M";
         };
       };
       behaviour = {
