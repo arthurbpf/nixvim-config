25edd5f4bede70b128cc72cff129aa09b119c576
Arthur Fernandes <arthurbpfernandes@gmail.com>
2025-10-31 08:36:13 -0300

    refactor: renaming of options to new pattern and conformity to stylix checks

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -1,65 +1,69 @@
 {
-  plugins.treesitter = {
-    enable = true;
-    folding = false;
-    nixvimInjections = true;
+  plugins = {
+    treesitter = {
+      enable = true;
+      folding = false;
+      nixvimInjections = true;
 
-    settings = {
-      indent = {
-        enable = true;
+      settings = {
+        indent = {
+          enable = true;
+        };
       };
     };
-  };
 
-  plugins.treesitter-context = {
-    enable = true;
-  };
+    treesitter-context = {
+      enable = true;
+    };
 
-  plugins.treesitter-textobjects = {
-    enable = false;
-    select = {
-      enable = true;
-      lookahead = true;
-      keymaps = {
-        "aa" = "@parameter.outer";
-        "ia" = "@parameter.inner";
-        "af" = "@function.outer";
-        "if" = "@function.inner";
-        "ac" = "@class.outer";
-        "ic" = "@class.inner";
-        "ii" = "@conditional.inner";
-        "ai" = "@conditional.outer";
-        "il" = "@loop.inner";
-        "al" = "@loop.outer";
-        "at" = "@comment.outer";
-      };
-    };
-    move = {
-      enable = true;
-      gotoNextStart = {
-        "]m" = "@function.outer";
-        "]]" = "@class.outer";
-      };
-      gotoNextEnd = {
-        "]M" = "@function.outer";
-        "][" = "@class.outer";
-      };
-      gotoPreviousStart = {
-        "[m" = "@function.outer";
-        "[[" = "@class.outer";
-      };
-      gotoPreviousEnd = {
-        "[M" = "@function.outer";
-        "[]" = "@class.outer";
-      };
-    };
-    swap = {
-      enable = true;
-      swapNext = {
-        "<leader>a" = "@parameters.inner";
-      };
-      swapPrevious = {
-        "<leader>A" = "@parameter.outer";
+    treesitter-textobjects = {
+      enable = false;
+      settings = {
+        select = {
+          enable = true;
+          lookahead = true;
+          keymaps = {
+            "aa" = "@parameter.outer";
+            "ia" = "@parameter.inner";
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "ac" = "@class.outer";
+            "ic" = "@class.inner";
+            "ii" = "@conditional.inner";
+            "ai" = "@conditional.outer";
+            "il" = "@loop.inner";
+            "al" = "@loop.outer";
+            "at" = "@comment.outer";
+          };
+        };
+        move = {
+          enable = true;
+          goto_next_start = {
+            "]m" = "@function.outer";
+            "]]" = "@class.outer";
+          };
+          goto_next_end = {
+            "]M" = "@function.outer";
+            "][" = "@class.outer";
+          };
+          goto_previous_start = {
+            "[m" = "@function.outer";
+            "[[" = "@class.outer";
+          };
+          goto_previous_end = {
+            "[M" = "@function.outer";
+            "[]" = "@class.outer";
+          };
+        };
+        swap = {
+          enable = true;
+          swap_next = {
+            "<leader>a" = "@parameters.inner";
+          };
+          swap_previous = {
+            "<leader>A" = "@parameter.outer";
+          };
+        };
       };
     };
   };
